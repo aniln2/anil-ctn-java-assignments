36290b742cff2790c339fbeffab936f81cd6c569
Anil Nayak <AnilN@emids.com>
2021-07-14 11:19:13 +0530

    spring project for practice

---
+++ src/test/java/com/emids/ctn/java8/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emids.ctn.java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
